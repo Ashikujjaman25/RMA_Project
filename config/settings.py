95b1fb30f8d00d40d174b4aeffe8e0db9cd0819d
Ashikujjaman <ashikashikujjaman76@gmail.com>
2026-06-30 22:37:58 +0600

    Create project structure and apps

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,22 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'apps.accounts',
+    'apps.departments',
+    'apps.reports',
+    'apps.tasks',
+    'apps.notifications',
+    'apps.dashboard',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
