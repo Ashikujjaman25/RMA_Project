60065590848b84cdc1c102dd87e36b20396bfd69
Ashikujjaman <ashikashikujjaman76@gmail.com>
2026-07-01 16:06:45 +0600

    Configure PostgreSQL database

--- config/settings.py
+++ config/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rma_db',
+        'USER': 'rma_user',
+        'PASSWORD': 'RMA@2026',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
