57aeb1125304e6966e9c97302582c1f297c146f4
Ashikujjaman <ashikashikujjaman76@gmail.com>
2026-07-02 16:20:27 +0600

    Implement custom login authentication

---
+++ apps/accounts/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter username",
+            }
+        ),
+    )
+
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter password",
+            }
+        ),
+    )
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from .forms import LoginForm
 
-# Create your views here.
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+
+    form = LoginForm(request, data=request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        login(request, form.get_user())
+        return redirect("/")
+
+    return render(request, "accounts/login.html", {"form": form})
